d2205730392796f43d869964f0abc7531d5be06e
Natalie Kovalchuk <koval4yk@hotmail.com>
2023-02-08 21:58:41 +0100

    second selenium lesson

--- src/test/java/FirstTestsGoogleTest.java
+++ src/test/java/FirstTestsGoogleTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -29,7 +30,21 @@ public class FirstTestsGoogleTest {
     public void shouldBeRightTitle(){
         title = driver.getTitle();
         Assert.assertEquals(title, "Google","The page contains a wrong title");
+    }
 
+    @Test
+    public void searchGoogle() {
+        driver.findElement(By.id("L2AGLb")).click();
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("Tel-Ran");
+        driver.findElement(By.name("btnK")).click();
+
+        Assert.assertTrue(driver.getPageSource().contains("Tel-Ran.de"), "The expected text was not in the search page");
+
+        String actualText = driver.findElement(By.id("hdtb-tls")).getText();
+        String expectedText = "Инструменты";
+
+        Assert.assertEquals(actualText, expectedText, "Фактический текст не соответствует ожидаемому");
     }
 
 
--- src/test/java/PhoneBookFirstTest.java
+++ src/test/java/PhoneBookFirstTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -16,7 +17,7 @@ public class PhoneBookFirstTest {
     public void setUp(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("http://phonebook.telran-edu.de:8080/user/login");
     }
 
@@ -24,6 +25,20 @@ public class PhoneBookFirstTest {
     public void shouldBeRightTitle() {
         title = driver.getTitle();
         Assert.assertEquals(title, "PhonebookUi", "The page contains a wrong title");
+    }
+
+    @Test
+    public void loginTest() {
+        //driver.findElement(By.id("L2AGLb")).click();
+        driver.findElement(By.name("email")).clear();
+        driver.findElement(By.name("email")).sendKeys("test@gmail.com");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("test@gmail.com");
+        driver.findElement(By.className("btn")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//button[contains(text(), 'Bakanov ')]")).isDisplayed());
+
+
 
     }
 
